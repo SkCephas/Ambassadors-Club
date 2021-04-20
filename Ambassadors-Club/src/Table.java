5e494f7a71dffda904bb0d149bca12893cabbb91
ManuelDCoder <ekwabena57@gmail.com>
2021-04-20 02:00:08 +0000

    Added The Table Form

---
+++ Ambassadors-Club/src/Table.java
@@ -0,0 +1,14 @@
+import javax.swing.*;
+
+public class Table {
+    private JPanel tablePanel;
+    private JPanel searchPanel;
+    private JPanel updateDeletePanel;
+    private JScrollPane tableScrollPanel;
+    private JTable table1;
+    private JTextField searchTextField;
+    private JButton searchButton;
+    private JLabel enterNameLabel;
+    private JButton updateButton;
+    private JButton deleteButton;
+}
